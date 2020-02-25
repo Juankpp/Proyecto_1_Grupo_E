4e7cf396216db5e775cca1d3ae4b7ee9a4be86d4
Guillermo Serrato <Guillermo Serrato@172.16.71.183>
2020-02-25 16:05:36 -0500

    Metodo de separacion de palabras por Split

--- src/co/edu/unbosque/controlador/Controlador.java
+++ src/co/edu/unbosque/controlador/Controlador.java
@@ -1,13 +1,14 @@
 package co.edu.unbosque.controlador;
 
+import co.edu.unbosque.persistence.Persistencia;
 import co.edu.unbosque.view.Iniciar;
 import co.edu.unbosque.view.VentanaPrincipal;
 
 public class Controlador {
 	
 	
-	Iniciar i = new Iniciar(this);
-	VentanaPrincipal vi = new VentanaPrincipal(this);	
-	
+	//Iniciar i = new Iniciar(this);
+	//VentanaPrincipal vi = new VentanaPrincipal(this);	
+	Persistencia pr = new Persistencia();
 	
 }
--- src/co/edu/unbosque/persistence/Persistencia.java
+++ src/co/edu/unbosque/persistence/Persistencia.java
@@ -1,24 +1,70 @@
 package co.edu.unbosque.persistence;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Persistencia {
 
-	private File ruta;
+	public static void main(String[] args) {
+		Leer_Archivo acceder = new Leer_Archivo();
+		acceder.leeArchivo();
+	}
+}
 
-	
-	public Persistencia() {
-		// TODO Auto-generated constructor stub
-		
-		ruta = new File("src/co/edu/unbosque/Util/Palabras.txt");
-		
-	
+class Leer_Archivo {
+	public static String linea;
+
+	public void leeArchivo() {
+
+		try {
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda
+			File archivo = new File(
+					"C:\\Users\\Guillermo Serrato\\git\\Proyecto_1_Grupo_E\\src\\co\\edu\\unbosque\\util\\Palabras.txt");
+			BufferedReader br = new BufferedReader(new FileReader(archivo));
+			linea = br.readLine();
+			String palabra = linea.toString();
+			String[] palabra1 = palabra.split(" ");
+			for (int i = 0; i < palabra1.length; i++) {
+				System.out.println(palabra1[i]);
+			}
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "No se encotro el archivo", "E R R O R", JOptionPane.ERROR_MESSAGE);
+		}
 	}
-	
-	public void escribirArchivo() {
-		
-		
+}
+
+class Escribir_Archivo {
+	public void escribir() {
+		FileWriter archivo = null;
+		PrintWriter dato = null;
+
+		Scanner leer = new Scanner(System.in);
+
+		try {
+			archivo = new FileWriter(
+					"C:\\Users\\Guillermo Serrato\\git\\Proyecto_1_Grupo_E\\src\\co\\edu\\unbosque\\util\\Adivinar.txt");
+			dato = new PrintWriter(archivo);
+
+			String cadena = JOptionPane.showInputDialog(null, "Ingrese datos: ");
+			dato.println(cadena);
+			dato.close();
+
+			JOptionPane.showMessageDialog(null, "Se escribio correctamente el archivo");
+
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "No se encotro el archivo", "E R R O R", JOptionPane.ERROR_MESSAGE);
+		} finally {
+			try {
+				if (null != archivo)
+					archivo.close();
+			} catch (Exception e2) {
+				JOptionPane.showMessageDialog(null, "No se encotro el archivo", "E R R O R", JOptionPane.ERROR_MESSAGE);
+			}
+
+		}
 	}
-	
-	
 }
